cb8a4563139a2d4f046864cd1407dfe73f9d9f78
Jerd <inquisitivequin@gmail.com>
2018-05-22 21:08:51 -0700

    mochi

--- test/test_helper.js
+++ test/test_helper.js
@@ -2,7 +2,13 @@ const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost/users_test')
 mongoose.connection
-	.once('open', () => console.log('Good'))
+	.once('open', () => console.log('Pass Pass'))
 	.on ('error', (error) => {
 		console.warn('Look out fool', error)
+	})
+
+	beforeEach((done) => {
+		mongoose.connection.collections.users.drop(() => {
+		done();
+		})
 	})
