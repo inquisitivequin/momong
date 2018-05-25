a80a2bfcd69591d4c63a97b0e1bd3946e5a26755
Jerd <inquisitivequin@gmail.com>
2018-05-25 12:22:26 -0700

    newnew

--- test/create_test.js
+++ test/create_test.js
@@ -2,9 +2,16 @@ const assert = require('assert');
 const User = require('../src/user')
 
 describe('Creating records', () => {
-	it('saves a user', () => {
+	it('saves a user', (done) => {
 		const jurd = new User({ name: 'Jurd' })
 
-		jurd.save();
+		jurd.save()
+		.then(() => {
+			assert(!jurd.isNew)
+			done()
+		})
+		.catch(() => {
+			console.log(err)
+		});
 	})
 })
